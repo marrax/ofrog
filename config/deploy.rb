41b14c33b7835a3323bc6b905ec7c0d8a805675f
marrax <marrax@gmail.com>
2015-03-17 13:53:05 +0000

    more cap stuff

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,15 +27,15 @@ set :rvm_type, :user
 # Default value for :linked_files is []
 #set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
 set :linked_files, %w{config/database.example.yml}
+set :keep_releases, 5
 
 # Default value for linked_dirs is []
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+#set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 
-# Default value for keep_releases is 5
-set :keep_releases, 5
 
 set(:config_files, %w(
   nginx.conf
@@ -61,8 +61,17 @@ set(:symlinks, [
 
 ])
 
-
 namespace :deploy do
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      # Your restart mechanism here, for example:
+      # execute :touch, release_path.join('tmp/restart.txt')
+    end
+  end
+
+  after :publishing, :restart
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
@@ -72,5 +81,19 @@ namespace :deploy do
       # end
     end
   end
+after 'deploy:symlink:shared' , 'deploy:compile_assets_locally'
 
 end
+
+#namespace :deploy do
+#
+#  after :restart, :clear_cache do
+#    on roles(:web), in: :groups, limit: 3, wait: 10 do
+#      # Here we can do anything such as:
+#      # within release_path do
+#      #   execute :rake, 'cache:clear'
+#      # end
+#    end
+#  end
+
+#end
