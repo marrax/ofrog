0ed628c5928ba1da86fcbdbf9747a2f65d2669e8
marrax <marrax@gmail.com>
2015-05-12 12:10:39 +0100

    live backup script

---
+++ backup_live.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+date=`date +%d-%m-%Y`
+
+rm -rf tmp/cache
+tar -cvzf ~/backups/ofrog-2.4.6-$date.tar.gz .
+pg_dump ofrog_prod -U ofroglive  > ~/backups/ofrog_stage.dump.$date
+#git add --all
+#git commit -m $date 
+#git push -u origin master
