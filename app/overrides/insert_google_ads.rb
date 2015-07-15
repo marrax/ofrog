68fda08ebf15b67982847e13ac54992f2116db85
marrax <marrax@gmail.com>
2015-07-15 10:43:03 +0100

    move google ads

--- app/overrides/insert_google_ads.rb
+++ app/overrides/insert_google_ads.rb
@@ -1,6 +1,6 @@
 Deface::Override.new(:virtual_path => 'spree/products/show',
           :name => 'insert_google_ads',
-          :insert_after => "[data-hook='promotions']",
+          :surround => "[data-hook='cart_form']",
           :partial => './layouts/google_ads.html.erb',
           :original => '659f89ff4b581ea4db26a818437f86d63929198e'
          )
